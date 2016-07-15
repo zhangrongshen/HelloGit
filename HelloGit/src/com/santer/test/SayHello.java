97b52391a6d233387e05cca984d7de14e50126f6
santer.zhang <santer@126.com>
2016-07-15 11:22:13 +0800

    add nihao test

--- HelloGit/src/com/santer/test/SayHello.java
+++ HelloGit/src/com/santer/test/SayHello.java
@@ -4,5 +4,6 @@ public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("Hello world") ;
 		System.out.println("ÄãºÃ");
+		System.out.println("Hello World!!") ;
 	}
 }
