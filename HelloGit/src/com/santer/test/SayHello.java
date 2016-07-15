87bdff0824a335ab8661d42d946e70a995e66377
santer.zhang <santer@126.com>
2016-07-15 11:29:34 +0800

    revert

--- HelloGit/src/com/santer/test/SayHello.java
+++ HelloGit/src/com/santer/test/SayHello.java
@@ -3,7 +3,6 @@ package com.santer.test;
 public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("Hello world") ;
-		System.out.println("ÄãºÃ");
 		System.out.println("Hello World!!") ;
 	}
 }
