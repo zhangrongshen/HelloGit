b459872e600008e109ff3c9cb49defa16a088ed8
santer.zhang <santer@126.com>
2016-07-15 11:42:15 +0800

    add

--- HelloGit/src/com/santer/test/SayHello.java
+++ HelloGit/src/com/santer/test/SayHello.java
@@ -4,5 +4,7 @@ public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("Hello world") ;
 		System.out.println("Hello World!!") ;
+		System.out.println("Hello World!!") ;
+		
 	}
 }
