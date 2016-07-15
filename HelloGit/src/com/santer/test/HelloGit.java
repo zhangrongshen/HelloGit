6ae6a6f24cab71ed90c3b829069b6368612e0165
santer.zhang <santer@126.com>
2016-07-15 09:55:59 +0800

    Test

--- HelloGit/src/com/santer/test/HelloGit.java
+++ HelloGit/src/com/santer/test/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 	public static void main(String[] args ) {
 		System.out.println("This is my first Git project") ;
 		System.out.println("Hello Git");
+		System.out.println("finish!") ;
 	}
 }
